b61b1dc33caaf098d28367d84c4b5782f023f124
Zuriel Valencia <valenciavvz0@gmail.com>
2026-07-18 16:04:36 -0600

    Creacion del App-Token

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const verificarTokenGenerico = (req, res, next) => {
+    const token = req.headers['app-token'];
+    const decoded = jwt.decode(token);
+
+    if (!token || !decoded || token !== process.env.App_token) {
+        return res.status(401).json({ message: "Acceso denegado." });
+    }
+
+    next();
+};
+
+module.exports = verificarTokenGenerico;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.js");
+const verificarTokenGenerico = require('../middleware/authMiddleware');
 
 // Rutas para la URL base: /api/v2/users
 router.route("/")
-    .post(userController.createUser)     // C - Crear
-    .get(userController.getAllUsers);    // R - Leer todos
+    .post(verificarTokenGenerico, userController.createUser)     
+    .get(verificarTokenGenerico, userController.getAllUsers);    
 
 // Rutas que necesitan un parámetro ID: /api/v2/users/:id
 router.route("/:id")
-    .get(userController.getUserById)     // R - Leer uno por ID
-    .put(userController.updateUser)      // U - Actualizar datos o desbloquear intentos
-    .delete(userController.deleteUser);  // D - Eliminar
+    .get(verificarTokenGenerico, userController.getUserById)     
+    .put(verificarTokenGenerico, userController.updateUser)      
+    .delete(verificarTokenGenerico, userController.deleteUser);  
 
 module.exports = router;
