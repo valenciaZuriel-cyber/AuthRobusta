d71e2178e12ce9bf4d2fbcce38c8f94fab119706
Zuriel Valencia <valenciavvz0@gmail.com>
2026-07-20 23:29:24 -0600

    Implementacion de Helmet

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const connectDB = require('./src/config/database.js');
 const express = require('express');
+const helmet = require('helmet'); // Importar Helmet
+
 const app = express();
 const port = process.env.PORT || 5100;
 const userRoutes = require("./src/routes/user.js");
 
+// Activar las protecciones de Helmet al inicio
+app.use(helmet());
 
 connectDB();
 
@@ -13,5 +17,4 @@ app.use("/api/v2/users", userRoutes);
 
 app.listen(port, () => {
   console.log("Hello world");
-
 });
